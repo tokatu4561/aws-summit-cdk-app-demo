000e8c575cbb3f098d4083b8441a9fff66034b10
tokatu4561 <ryobsk4561@gmail.com>
2024-05-03 21:42:35 +0900

    add pipeline

--- bin/aws-summit-cdk-app-demo.ts
+++ bin/aws-summit-cdk-app-demo.ts
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { BackendStack } from '../lib/backend-stack';
-import { ApiStack } from '../lib/api-stack';
-import { FrontendStack } from '../lib/frontend-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
 const prefix = 'AwsSummitCdkAppDemo';
 
-const backend = new BackendStack(app, `${prefix}Backend`);
-new ApiStack(app, `${prefix}API`, {
-  currentTable: backend.currentTable,
+const repo = app.node.tryGetContext('source-repository');
+const branch = app.node.tryGetContext('source-branch');
+const connectionArn = app.node.tryGetContext('source-connection-arn');
+
+new PipelineStack(app, `${prefix}Pipeline`, {
+  prefix,
+  repo,
+  branch,
+  connectionArn,
 });
-new FrontendStack(app, `${prefix}Frontend`);
+
+// const backend = new BackendStack(app, `${prefix}Backend`);
+// new ApiStack(app, `${prefix}API`, {
+//   currentTable: backend.currentTable,
+// });
+// new FrontendStack(app, `${prefix}Frontend`);
